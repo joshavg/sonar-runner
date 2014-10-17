118a82fc055ff1d2d4f63448ba32880cf82dc9c3
neuralys <nico@neuralys.com>
2014-10-17 16:46:27 +0200

    apply from v2.4

--- bin/sonar-runner.bat
+++ bin/sonar-runner.bat
@@ -1,4 +1,4 @@
-@REM Sonar Runner Startup Script for Windows
+@REM SonarQube Runner Startup Script for Windows
 @REM
 @REM Required ENV vars:
 @REM   JAVA_HOME - location of a JDK home dir
@@ -9,6 +9,10 @@
 
 @echo off
 
+set ERROR_CODE=0
+
+@REM set local scope for the variables with windows NT shell
+@setlocal
 
 @REM ==== START VALIDATION ====
 @REM *** JAVA EXEC VALIDATION ***
@@ -24,12 +28,12 @@ if not "%JAVA_EXEC%" == "" (
 if not "%JAVA_EXEC%" == "" goto OkJava
 
 echo.
-echo ERROR: JAVA_HOME not found in your environment, and no Java 
+echo ERROR: JAVA_HOME not found in your environment, and no Java
 echo        executable present in the PATH.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation, or add "java.exe" to the PATH
 echo.
-goto end
+goto error
 
 :foundJavaHome
 if EXIST "%JAVA_HOME%\bin\java.exe" goto foundJavaExeFromJavaHome
@@ -38,7 +42,7 @@ echo.
 echo ERROR: JAVA_HOME exists but does not point to a valid Java home
 echo        folder. No "\bin\java.exe" file can be found there.
 echo.
-goto end
+goto error
 
 :foundJavaExeFromJavaHome
 set JAVA_EXEC="%JAVA_HOME%\bin\java.exe"
@@ -60,7 +64,7 @@ echo.
 echo ERROR: SONAR_RUNNER_HOME exists but does not point to a valid install
 echo        directory: %SONAR_RUNNER_HOME%
 echo.
-goto end
+goto error
 
 
 
@@ -70,9 +74,24 @@ echo %SONAR_RUNNER_HOME%
 
 set PROJECT_HOME=%CD%
 
-%JAVA_EXEC% %SONAR_RUNNER_OPTS% -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar";"%SONAR_RUNNER_HOME%\lib\sonar-batch-bootstrapper.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" "-Dproject.home=%PROJECT_HOME%" org.sonar.runner.Main %*
+%JAVA_EXEC% %SONAR_RUNNER_OPTS% -cp "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" "-Dproject.home=%PROJECT_HOME%" org.sonar.runner.Main %*
+if ERRORLEVEL 1 goto error
+goto end
 
-
+:error
+set ERROR_CODE=1
 
 @REM ==== END EXECUTION ====
+
 :end
+@REM set local scope for the variables with windows NT shell
+@endlocal & set ERROR_CODE=%ERROR_CODE%
+
+@REM see http://code-bear.com/bearlog/2007/06/01/getting-the-exit-code-from-a-batch-file-that-is-run-from-a-python-program/
+goto exit
+
+:returncode
+exit /B %1
+
+:exit
+call :returncode %ERROR_CODE%
